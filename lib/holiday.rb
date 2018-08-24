e55ed75f57b49887bc4d1b4d62bb32a64700922c
John Grubbs <johnmlowery@gmail.com>
2018-08-24 00:10:02 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -20,7 +20,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   # return the second element in the 4th of July array
   holiday_supplies.each do |season, day|
     :summer.each do |key, string|
-       if attribute == 
+       if attribute == :fourth_of_july 
     end
   end
 end
